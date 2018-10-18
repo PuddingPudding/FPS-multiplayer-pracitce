13596c35963d344ff325868231d48d0fd06e5af3
PuddingPudding <hwsh70119@gmail.com>
2018-10-19 00:21:05 +0800

    完成連線教學，同時也去試試了Unity官方的多人連線服務

---
+++ Assets/Scripts/Network Scripts/NetManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetManager : NetworkManager //繼承我們一般使用的Unet網路管理者
+{
+    private bool m_bFirstPlayerJoined = false;
+
+    public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
+    {
+        GameObject playerObj = Instantiate(playerPrefab , Vector3.zero , Quaternion.identity);
+        //playerObj.SetActive(true);
+        List<Transform> listSpawnPos = NetworkManager.singleton.startPositions; //將所有出生點取到list當中
+
+        if(!m_bFirstPlayerJoined)
+        {
+            m_bFirstPlayerJoined = true;
+            playerObj.transform.position = listSpawnPos[0].position;
+        }
+        else
+        {
+            playerObj.transform.position = listSpawnPos[1].position;
+        }
+
+        NetworkServer.AddPlayerForConnection(conn, playerObj, playerControllerId);
+
+    }
+
+    void SetPortAndAddress()
+    {
+        NetworkManager.singleton.networkPort = 7777;
+        NetworkManager.singleton.networkAddress = "localhost";
+    }
+
+    public void HostGame()
+    {
+        this.SetPortAndAddress();
+        NetworkManager.singleton.StartHost();
+    }
+
+    public void JoinGame()
+    {
+        SetPortAndAddress();
+        NetworkManager.singleton.StartClient();
+    }
+
+}
